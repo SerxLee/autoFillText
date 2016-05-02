9ad249e30f0b15f5cc8017f8e5e048cae71a8ab2
SerxLee <serx.lee@gmail.com>
2016-05-02 15:34:04 +0800

    fix

--- autoFillText/ViewController.swift
+++ autoFillText/ViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 import Foundation
 import WebKit
 
-let stackHeightShow:CGFloat = 150.0
-let stackHeighthide:CGFloat = 0
 
 class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource{
     
@@ -20,44 +18,39 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
     let recordData = RecordData()
 
     @IBOutlet weak var complectedTableView: UITableView!
-    
     @IBOutlet weak var testTextField: UITextField!
     
-    @IBOutlet weak var stackViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var secondTextFileToFirst: NSLayoutConstraint!
+    @IBOutlet weak var tableViewHeight: NSLayoutConstraint!
+    
     var showList: Bool = false
     
     var dataSource: [String] = []
     var resultData: [String] = []
+    
+    
     //MARK: - ----Apple Inc. func----
     //MARK: -
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        showList = false
-        
         getRecordData()
-        
         loadUI()
-        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
         let cell = tableView.cellForRowAtIndexPath(indexPath)
         self.testTextField.text = cell?.textLabel?.text
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        UIView.animateWithDuration(0.3){
-
-            self.stackViewHeight.constant = stackHeighthide
-            self.complectedTableView.hidden = true
-
-        }
+        self.complectedTableView.hidden = true
+        hideSecondTextFeild()
     }
     
     //tableview datasource
@@ -69,45 +62,55 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
         
         let identifier = "cell"
         let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath) as UITableViewCell
-        
         cell.textLabel?.text = resultData[indexPath.row]
         return cell
     }
     
+    
+    
     //MARK: - ----individual func----
     //MARK: -
+    
+    
     
     func didChange(textField: UITextField){
         resultData = []
         print(textField.text)
+        
         let currentString = textField.text!
-        if currentString.isEmpty{
+        if currentString.isEmpty {
             showList = false
-            UIView.animateWithDuration(0.3){
-                self.complectedTableView.hidden = !self.showList
-                self.stackViewHeight.constant = stackHeighthide
-            }
-        }else{
+            self.complectedTableView.hidden = true
+            hideSecondTextFeild()
+        }
+        else {
             complectedTableView.hidden = true
+            
             let myMatch = UserNameMatch(array: dataSource, string: currentString)
+            
             resultData = myMatch.sl_matchlist()
-            if resultData.count > 0{
+            if resultData.count > 0 {
+                
                 print(resultData)
                 showList = true
-            }else{
-                showList = false
-            }
-            
-            UIView.animateWithDuration(0.3){
-                self.complectedTableView.reloadData()
-                self.complectedTableView.hidden = !self.showList
-                if self.showList{
-                    self.stackViewHeight.constant = stackHeightShow
-                }else{
-                    self.stackViewHeight.constant = stackHeighthide
-
+                if resultData.count <= 5 {
+                    let lim = HeightForCell * CGFloat(resultData.count)
+                    tableViewHeight.constant = lim
+                    secondTextFileToFirst.constant = lim
+                }
+                else{
+                    tableViewHeight.constant = HeightForCell * 5.0
+                    secondTextFileToFirst.constant = HeightForCell * 5.0
                 }
             }
+            else {
+                showList = false
+                hideSecondTextFeild()
+
+            }
+            self.complectedTableView.reloadData()
+            self.complectedTableView.hidden = !self.showList
+
         }
 
     }
@@ -119,28 +122,36 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
 
     
     func loadUI(){
+        tableViewHeight.constant = 0.0
+        hideSecondTextFeild()
+        
+        showList = false
         
         testTextField.delegate = self
         testTextField.addTarget(self, action: #selector(ViewController.didChange(_:)), forControlEvents: .EditingChanged)
         
         complectedTableView.dataSource = self
         complectedTableView.delegate = self
-        complectedTableView.tableFooterView = UIView.init()
-        complectedTableView.layer.borderWidth = 0.5
-        complectedTableView.layer.borderColor = UIColor.blackColor().CGColor
         
-        self.stackViewHeight.constant = stackHeighthide
         complectedTableView.hidden = !showList
     }
+    
     @IBAction func uploadText(sender: UIButton) {
         
         let limStr = self.testTextField.text!
         if !limStr.isEmpty{
-            dataSource.append(limStr)
             
+            dataSource.append(limStr)
             let limMutableArray = NSMutableArray(array: dataSource)
             recordData.toWriteArray = limMutableArray
             recordData.dataWrite()
         }
     }
+    
+    func hideSecondTextFeild(){
+        self.secondTextFileToFirst.constant = rewHeightForSecondToFirst
+    }
 }
+
+let HeightForCell: CGFloat = 30.0
+let rewHeightForSecondToFirst: CGFloat = 8.0
---
+++ autoFillText/testAgain.swift
@@ -0,0 +1,14 @@
+//
+//  testAgain.swift
+//  autoFillText
+//
+//  Created by Serx on 16/4/18.
+//  Copyright © 2016年 serxlee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class textFieldView: NSObject{
+
+}
